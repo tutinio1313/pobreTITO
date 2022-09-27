57c777602b1ce5c79575a1371ad24b27a8dc8230
Andrés Rossini <tutinio1313@gmail.com>
2022-09-27 20:04:42 -0300

    Added documentation.

--- Models/Report.cs
+++ Models/Report.cs
@@ -4,7 +4,7 @@ namespace pobreTITO_Models
     public class Report
     {
         public int id { get; set; }
-        public string location { get; set; }
+        public string address { get; set; }
         public ReportType.reportType type { get; set; }
         public string comment { get; set; }
         public string imageURL { get; set; }
